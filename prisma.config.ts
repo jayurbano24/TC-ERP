9cc1b32c8280c8b72dfc0d175908386dac6a1892
UrbanoTCW24 <gurbanotcwmx@gmail.com>
2026-06-14 01:50:42 -0600

    fix(build): @ts-ignore en prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@prisma/config';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: '.env.local' });
 export default defineConfig({
+  // @ts-ignore - Required for early access features but not in the type definitions yet
   earlyAccess: true,
   schema: 'src/infrastructure/database/prisma/schema.prisma',
   datasource: {
